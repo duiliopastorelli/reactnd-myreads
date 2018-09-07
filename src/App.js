d972619fa2770e77529140ec25ab489fcb114791
Danilo Mezgec <mezgec.danilo@gmail.com>
2018-09-07 18:48:07 +0200

    chore: remove unused state prop from App.js
    This prop is unused because the Router is taking care of switch the views between main and search

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import SearchBar from "./SearchBar";
 import { Route, Link } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-
-    //TODO: remove this prop app-wide
-    showSearchPage: false
-  };
-
   render() {
     return (
       <div className="app">
